4cb3ef73a9053e4372faf01134ed52423f02374f
Ashritha <ashritha030@gmail.com>
2024-07-16 18:53:17 +0530

    adding program

---
+++ ForLoopgit.java
@@ -0,0 +1,15 @@
+package basicknowledge;
+
+public class ForLoopgit {
+
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("AshrithaYashwanth");
+		}
+
+
+	}
+
+}
